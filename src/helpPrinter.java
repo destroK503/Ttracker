39597e6cb315519d19adff2321f4c2a14a92b7ff
klkgang <139710771+klkgang@users.noreply.github.com>
2025-03-10 12:30:56 -0600

    [ENTRY_POINT] made some changes to the help outpout

--- src/helpPrinter.java
+++ src/helpPrinter.java
@@ -1,9 +1,14 @@
 public class helpPrinter {
-    public static void print() {
-        String str = """
-            Ttracker use pending to print the tasks that are pending
-            """;
-        System.out.println(str);
-    }
-}
+  private static String introduction = "Ttracker; The not perfect tracker for your tasks\n";
+  private static String options = String.format(
+      "%s - %s%n",
+      "pending", "Will show the tasks that are as pending in the tasks stock");
 
+  public static void print() {
+    String str = String.format(
+        "%s---%n%s",
+        introduction,
+        options);
+    System.out.print(str);
+  }
+}
